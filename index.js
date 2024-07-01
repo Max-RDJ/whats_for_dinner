681e06dad010d0499a9dc64c1648b44714dc9728
Max-RDJ <maxjeremiahgd@gmail.com>
2024-07-01 21:20:52 +0200

    Progress
    New meal ideas added via input come out as "undefined"

--- index.js
+++ index.js
@@ -38,9 +38,68 @@ const meals =  [
     }
 ]
 
-// Select random number associated with meal in meals array
-// Create loop for number of meals selected from dropdown
-mealsNumber = document.getElementById("meals-number");
+// Initialise
+    let mealInput = document.getElementById("mealInput");
+    let meal_list = localStorage.getItem("meals") ? JSON.parse(localStorage.getItem("meals")).meals : ["sd"];
+
+function paintUI() {
+    let new_inner_html = "";
+    for (let i = 0; i < meals.length; i++) {
+        const meal = meals[i];
+        new_inner_html +=
+        `
+        <div class="mealItem">
+        <p>${meal}</p>
+        <div class="actionsContainer">
+        <button onclick="editMeal(${i})"><i class="fa-solid fa-pen-to-square"></i></button>
+        <button onclick="deleteMeal(${i})"><i class="fa-solid fa-trash"></i></button>
+        </div>
+        </div>
+        `
+    }
+    // mainContainer.innerHTML = new_inner_html;
+    saveData();
+}
+
+paintUI();
+
+
+// Add new meal idea
+function addMeal() {
+    let current_meal = mealInput.value;
+    if (!current_meal) {return};
+    meals.push(current_meal);
+    mealInput.value = "";
+    paintUI();
+}
+
+document.getElementById("add-btn").addEventListener("click", addMeal);
+
+
+// Delete a meal idea
+function deleteTodo(index) {
+    let new_meals = meals.filter((current_value, current_index) => {
+        return current_index !== index;
+    })
+    meals = new_meals;
+    paintUI()
+}
+
+
+// Edit a meal idea
+function editTodo(index) {
+    let current_meal = meal_list[index];
+    mealInput.value = current_meal;
+    deleteMeal(index)
+}
+
+// Step 5: Persist all info
+function saveData() {
+    localStorage.setItem("meals", JSON.stringify({meals}));
+}
+
+
+const mealsNumber = document.getElementById("meals-number");
 
 const dailyMeals = [
     document.getElementById("first-meal"),
@@ -52,19 +111,34 @@ const dailyMeals = [
     document.getElementById("seventh-meal")
 ];
 
-for (i = 0; i < mealsNumber; i++){
-    function getRandomMeal() {
-        let j = Math.floor(Math.random() * meals.length)
-        for (k = 0; k < dailyMeals.length; k++) {
-            dailyMeals[k].innerHTML = meals[j];
+// Select random number associated with meal in meals array
+function getRandomMeal(availableMeals) {
+    let randomMeal = Math.floor(Math.random() * availableMeals.length);
+    return availableMeals.splice(randomMeal, 1)[0];
+}
+
+function generateMeals() {
+    const numbersOfMeals = parseInt(mealsNumber.value);
+
+    let availableMeals = [...meals];
+
+    dailyMeals.forEach(mealElement => {
+        mealElement.innerHTML = '';
+    });
+
+    // Iterate through number of meals selected from dropdown
+    for (i = 0; i < numbersOfMeals; i++){
+    if (i < dailyMeals.length) {
+        if (availableMeals.length === 0) {
+            break;
         }
+        let selectedMeal = getRandomMeal(availableMeals);
+        dailyMeals[i].innerHTML = selectedMeal.mealName; 
+    }
     }
 }
 
-getRandomMeal()
+const genBtn = document.getElementById("generate-btn").addEventListener("click", generateMeals);
 
 // Add meal name to output in HTML
 // Increase number each time in for loop in order to move on to next row of text
-
-
-firstMeal.innerText = meals[j]
