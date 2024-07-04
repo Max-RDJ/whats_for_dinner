865daf51f1a6c639b80c9a65a7d5ce11f4ce1f16
Max-RDJ <maxjeremiahgd@gmail.com>
2024-07-04 16:40:58 +0100

    Some progress

--- index.js
+++ index.js
@@ -145,5 +145,6 @@ function openNav() {
 /* TODO:
 - Add number buttons to replace dropdown
 - Add animation to appearing meals
-- Make navbar 100% height of 
+- Make sidebar 100% height of everything below header and position below header
+- Dim rest of page when sidebar active
 */
